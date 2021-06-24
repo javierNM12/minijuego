a4c4bb66d5647c56051d5e27fdc2b8617df838e8
javierNM12 <javier.nunez.morales.98@gmail.com>
2021-06-24 19:39:22 +0200

    Aplicación funcionando correctamente

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+$(() => {
+    $('#si').hover(cambiarPosicion)
+});
+
+let cambiarPosicion = () => {
+
+    let top = Math.floor((Math.random() * 100) + 1) + '%';
+    let left = Math.floor((Math.random() * 100) + 1)+ '%';
+
+    $('#si').css({
+        top: top,
+        left: left,
+        position: 'absolute'
+    });
+}
